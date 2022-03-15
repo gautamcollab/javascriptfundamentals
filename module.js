1731f2964835e7099aefbb331e930bc47f23a6a7
Gautam Sinha <gautam.sinha@live.com>
2022-03-15 15:46:11 -0400

    website update

---
+++ module.js
@@ -0,0 +1,10 @@
+
+
+function quiz(){
+let myName = prompt('Enter your name')
+let month = prompt('What month were you born?')
+let food = prompt('What is your favorite food')
+console.log('My name is', myName)
+console.log('I was born in', month)
+console.log('My favorite food is', food)
+}
