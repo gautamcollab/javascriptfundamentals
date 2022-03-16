689a1b966dfaed0e991bd97bb2280ea476478041
Gautam Sinha <gautam.sinha@live.com>
2022-03-16 13:31:13 -0400

    added functions to rote

---
+++ rote.js
@@ -0,0 +1,18 @@
+const greet = () => {
+    console.log('Hello World!')
+}
+
+greet()
+
+const square = (num) => {
+    return num * num
+}
+
+square(25)
+
+const greetByName = (str) => {
+    console.log("Hello", str)
+}
+
+greetByName("Gautam")
+
