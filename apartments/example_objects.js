f1d0198a74ebc035dce7e31e19d840fd712874c3
Gautam Sinha <gautam.sinha@live.com>
2022-03-18 11:40:32 -0400

    example objects

---
+++ apartments/example_objects.js
@@ -0,0 +1,56 @@
+const laptop = {
+    price: 1000,
+    brand: 'HP', 
+    memory: '2GBs',
+    color: 'green', 
+}
+
+const car = {
+    engine: 'V8',
+    doors: 2, 
+    model: 'BMW',
+    price: 200000,
+    make: 'M4'
+}
+
+const company = {
+    name: 'Vittles',
+    CEO: 'Michael Law',
+    isTheBest: true,
+    publicyTraded: false
+}
+
+const room = {
+    windows: 0,
+    dimensions: '200x200', 
+    closet: false, 
+    isLegal: false, 
+    pets: false
+}
+
+const item = {
+    name: 'Excalibur',
+    attackPower: 130
+}
+
+const ally = {
+    name: 'Geralt',
+    power: {name: 'Kamehameha', attackPower: 9000}
+}
+
+const character = {
+    hairColor: 'blue', 
+    height: "6'7",
+    race: 'Elf',
+    gender: null,
+    strength: Infinity, 
+    dlc: false, 
+    class: 'Mage',
+    cohort: '031422',
+    item: item,
+    ally: ally
+}
+
+
+
+console.log(character)
